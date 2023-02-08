550a6a931f134990015d7418ed76b6ee67a7f0d8
iceiceice <svoikin.slava@yandex.ru>
2023-02-09 00:41:27 +0500

    Edit files

---
+++ README.txt
@@ -0,0 +1,5 @@
+# The program reads the current price of the XRP/USDT futures on the Binance exchange.
+# If the price has fallen by 1% from the maximum price in the last hour since the start
+# of work, the program displays the message
+# To the console. At the same time, the program continues to work further, constantly
+# reading the current price.
--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# The program reads the current price of the XRP/USDT futures on the Binance exchange.
-# If the price has fallen by 1% from the maximum price in the last hour since the start
-# of work, the program displays the message
-# To the console. At the same time, the program continues to work further, constantly
-# reading the current price.
 
 from keys import API_KEY, SECRET_KEY
 from binance.um_futures import UMFutures
